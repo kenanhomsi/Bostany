dd393f3cf52918b89164ef43b8420e8921efbeb0
kenanOrg <kenanhomsi0959@gmail.com>
2024-11-09 20:58:11 +0300

    from home to dashboard

--- src/pages/Website-Pages/Home.tsx
+++ src/pages/Website-Pages/Home.tsx
@@ -1,8 +1,22 @@
+import { useEffect } from "react"
 import MainHomeContent from "../../components/General/MainHomeContent"
 import About from "../../components/WebsiteCom/About"
 import Banner from "../../components/WebsiteCom/Banner"
+import { useAppSelector } from "../../redux/store"
+import { useNavigate } from "react-router-dom"
 
 const Home = () => {
+    const user = useAppSelector((state) => state.auth)
+    const navigate = useNavigate()
+    useEffect(() => {
+        if (user.token && user.isAuthenticated) {
+            if (user.user?.type == 'consultant') {
+                navigate('/Bostany')
+            } else {
+                navigate('/Baser')
+            }
+        }
+    }, [user])
     return (
         <div className="min-h-screen flex flex-col items-center gap-16 pb-24">
             <MainHomeContent place='website' />
